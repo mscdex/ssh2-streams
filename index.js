ea70185dbc1071aee2054c63f45071f64711447a
Brian White <mscdex@mscdex.net>
2014-11-15 13:51:55 -0500

    add constants to main exports

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 module.exports = {
   SFTPStream: require('./lib/sftp'),
   SSH2Stream: require('./lib/ssh'),
-  utils: require('./lib/utils')
+  utils: require('./lib/utils'),
+  constants: require('./lib/constants')
 };
